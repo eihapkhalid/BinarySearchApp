b2d963c1995e409aaafab81c3ce61ff5ceb679d7
eihapKhalid <eihapinfo@gmail.com>
2023-06-12 08:18:25 +0300

    Added the BinarySearchViewModel class to the Domains.ViewModels namespace.

---
+++ Domains/ViewModels/BinarySearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains.ViewModels
+{
+    public class BinarySearchViewModel
+    {
+        public int[] SearchArray { get; set; }
+        public int SearchElement { get; set; }
+    }
+}
