3a311d280d811cbcb22bb7f85cfb989492952406
eihapKhalid <eihapinfo@gmail.com>
2023-06-13 12:52:07 +0300

    update HomeController function  to serch word using IndexOf() Or  Number using BinarySearch

--- BinarySearchApp/Controllers/HomeController.cs
+++ BinarySearchApp/Controllers/HomeController.cs
@@ -22,28 +22,74 @@ namespace BinarySearchApp.Controllers
             {
                 if (!string.IsNullOrEmpty(model.SearchArray))
                 {
-                    string text = model.SearchArray;
-                    string word = model.SearchElement;
+                    string searchTerm = model.SearchElement;
+                    bool isNumericOrOrdered = IsNumericOrOrderedString(searchTerm);
 
-                    int result = text.IndexOf(word);
-
-                    if (result != -1)
+                    if (isNumericOrOrdered)
                     {
-                        ViewBag.Result = $"Word '{word}' found at index {result}";
+                        int[] array = model.SearchArray.Split(' ').Where(str => int.TryParse(str, out _)).Select(int.Parse).ToArray();
+                        int element = Convert.ToInt32(searchTerm);
+
+                        int result = BinarySearch(array, element);
+
+                        if (result != -1)
+                        {
+                            ViewBag.Result = $"Element {element} found at index {result}";
+                        }
+                        else
+                        {
+                            ViewBag.Result = $"Element {element} not found in the array";
+                        }
                     }
                     else
                     {
-                        ViewBag.Result = $"Word '{word}' not found in the text";
+                        string text = model.SearchArray;
+                        int result = text.IndexOf(searchTerm);
+
+                        if (result != -1)
+                        {
+                            ViewBag.Result = $"Word '{searchTerm}' found at index {result}";
+                        }
+                        else
+                        {
+                            ViewBag.Result = $"Word '{searchTerm}' not found in the text";
+                        }
                     }
                 }
             }
 
             return View(model);
         }
+
+        #region IsNumericOrOrderedString
+        private bool IsNumericOrOrderedString(string value)
+        {
+            bool isNumeric = value.All(char.IsDigit);
+            bool isOrdered = IsOrderedString(value);
+
+            return isNumeric || isOrdered;
+        }
+        #endregion
+
+        #region IsOrderedString
+        private bool IsOrderedString(string value)
+        {
+            for (int i = 1; i < value.Length; i++)
+            {
+                if (value[i] < value[i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        } 
+        #endregion
+
         #endregion
 
         #region BinarySearch
-        private int BinarySearch(string[] array, string element)
+        private int BinarySearch(int[] array, int element)
         {
             int left = 0;
             int right = array.Length - 1;
@@ -53,13 +99,13 @@ namespace BinarySearchApp.Controllers
             {
                 int mid = left + (right - left) / 2;
 
-                if (array[mid].Contains(element))
+                if (array[mid] == element)
                 {
                     result = mid;
                     break;
                 }
 
-                if (string.Compare(array[mid], element) < 0)
+                if (array[mid] < element)
                 {
                     left = mid + 1;
                 }
